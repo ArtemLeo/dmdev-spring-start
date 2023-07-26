f718477a9602e070946fa19834eab26a7bd728f7
Artem Andriushchenko <link.by.artem@gmail.com>
2023-07-26 17:22:58 +0300

    Add lesson Factory Method Injection

--- src/main/java/com/artemdev/spring/ApplicationRunner.java
+++ src/main/java/com/artemdev/spring/ApplicationRunner.java
@@ -1,24 +1,15 @@
 package com.artemdev.spring;
 
 import com.artemdev.spring.database.pool.ConnectionPool;
-import com.artemdev.spring.database.repository.CompanyRepository;
-import com.artemdev.spring.database.repository.UserRepository;
-import com.artemdev.spring.ioc.Container;
-import com.artemdev.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var container = new Container();
+        var context = new ClassPathXmlApplicationContext("application.xml");
 
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-        var connectionPool = container.get(ConnectionPool.class);
-        var userRepository = container.get(UserRepository.class);
-        var companyRepository = container.get(CompanyRepository.class);
-        var userService = container.get(UserService.class);
-        // TODO: 26.07.2023 userService
+        // clazz -> String -> Map<String,Object> - запрос на создание bean-объекта;
+        var connectionPool = context.getBean("pool-1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
+
