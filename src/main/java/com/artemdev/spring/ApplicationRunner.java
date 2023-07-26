2850acf57290c2fe49582fc9ea06b011f88ab6b3
Artem Andriushchenko <link.by.artem@gmail.com>
2023-07-27 02:46:36 +0300

    Add lesson Setter Method Injection

--- src/main/java/com/artemdev/spring/ApplicationRunner.java
+++ src/main/java/com/artemdev/spring/ApplicationRunner.java
@@ -6,14 +6,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 
-        // clazz -> String -> Map<String,Object> - запрос на создание bean-объекта;
-        var connectionPool = context.getBean("pool-1", ConnectionPool.class);
-        System.out.println(connectionPool);
+            // clazz -> String -> Map<String,Object> - запрос на создание bean-объекта;
+            var connectionPool = context.getBean("pool-1", ConnectionPool.class);
+            System.out.println(connectionPool);
 
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 }
 
